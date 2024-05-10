d38e5b4dfed4d252c641be61d8b885502c43ddfd
Anuj Tiwari <anuj.10tiwari2001@gmail.com>
2024-05-10 23:30:34 +0530

    Learned about injection of Collection & reference object with different writing ways

--- src/main/java/org/spring/Student.java
+++ src/main/java/org/spring/Student.java
@@ -1,10 +1,39 @@
 package org.spring;
 
+import java.util.Map;
+import java.util.List;
+
 public class Student {
     private int studentId;
     private String studentName;
     private String studentAddress;
+    private List<Integer> studentMarks;
+    private Map<String,Integer> studentConfig;
+    private Address studentPAddress;
 
+    public Address getStudentPAddress() {
+        return studentPAddress;
+    }
+
+    public void setStudentPAddress(Address studentPAddress) {
+        this.studentPAddress = studentPAddress;
+    }
+
+    public Map<String, Integer> getStudentConfig() {
+        return studentConfig;
+    }
+
+    public void setStudentConfig(Map<String, Integer> studentConfig) {
+        this.studentConfig = studentConfig;
+    }
+
+    public List<Integer> getStudentMarks() {
+        return studentMarks;
+    }
+
+    public void setStudentMarks(List<Integer> studentMarks) {
+        this.studentMarks = studentMarks;
+    }
 
     @Override
     public String toString() {
@@ -12,6 +41,9 @@ public class Student {
                 "studentId=" + studentId +
                 ", studentName='" + studentName + '\'' +
                 ", studentAddress='" + studentAddress + '\'' +
+                ", studentMarks=" + studentMarks +
+                ", studentConfig=" + studentConfig +
+                ", studentPAddress="+studentPAddress+
                 '}';
     }
 
